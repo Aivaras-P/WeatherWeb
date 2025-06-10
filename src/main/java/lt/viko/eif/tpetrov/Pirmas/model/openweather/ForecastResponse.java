2a5654bdcf6b38f8c8f3cbadaeaccca843fb56ce
Aivaras-P <s054117@ad.viko.lt>
2025-06-11 00:10:17 +0300

    Update ForecastResponse.java
    Padaryta, kad būtų galimą gauti kas valandinius oro temepratūros pakeitimus.

--- src/main/java/lt/viko/eif/tpetrov/Pirmas/model/openweather/ForecastResponse.java
+++ src/main/java/lt/viko/eif/tpetrov/Pirmas/model/openweather/ForecastResponse.java
@@ -3,7 +3,7 @@ package lt.viko.eif.tpetrov.Pirmas.model.openweather;
 import java.util.List;
 
 /**
- * Represents the 7-day weather forecast data from the OpenWeather API.
+ * Represents the weather forecast data from the OpenWeather API.
  */
 public class ForecastResponse {
 
@@ -13,12 +13,24 @@ public class ForecastResponse {
     private List<Daily> daily;
 
     /**
-     * Returns the list of daily forecasts.
-     *
-     * @return list of {@link Daily}
+     * A list of hourly weather forecast entries.
      */
+    private List<Hourly> hourly;
+
     public List<Daily> getDaily() {
         return daily;
+    }
+
+    public void setDaily(List<Daily> daily) {
+        this.daily = daily;
+    }
+
+    public List<Hourly> getHourly() {
+        return hourly;
+    }
+
+    public void setHourly(List<Hourly> hourly) {
+        this.hourly = hourly;
     }
 
     /**
@@ -30,40 +42,96 @@ public class ForecastResponse {
         private int humidity;
         private List<Weather> weather;
 
-        /**
-         * Returns the temperature data.
-         *
-         * @return {@link Temp}
-         */
         public Temp getTemp() {
             return temp;
         }
 
-        /**
-         * Returns the wind speed.
-         *
-         * @return wind speed in m/s
-         */
+        public void setTemp(Temp temp) {
+            this.temp = temp;
+        }
+
         public double getWind_speed() {
             return wind_speed;
         }
 
-        /**
-         * Returns the humidity level.
-         *
-         * @return humidity in percent
-         */
+        public void setWind_speed(double wind_speed) {
+            this.wind_speed = wind_speed;
+        }
+
         public int getHumidity() {
             return humidity;
         }
 
-        /**
-         * Returns the list of weather descriptions.
-         *
-         * @return list of {@link Weather}
-         */
+        public void setHumidity(int humidity) {
+            this.humidity = humidity;
+        }
+
         public List<Weather> getWeather() {
             return weather;
+        }
+
+        public void setWeather(List<Weather> weather) {
+            this.weather = weather;
+        }
+    }
+
+    /**
+     * Represents hourly forecast data.
+     */
+    public static class Hourly {
+        private long dt;
+        private double temp;
+        private double feels_like;
+        private int humidity;
+        private double wind_speed;
+        private List<Weather> weather;
+
+        public long getDt() {
+            return dt;
+        }
+
+        public void setDt(long dt) {
+            this.dt = dt;
+        }
+
+        public double getTemp() {
+            return temp;
+        }
+
+        public void setTemp(double temp) {
+            this.temp = temp;
+        }
+
+        public double getFeels_like() {
+            return feels_like;
+        }
+
+        public void setFeels_like(double feels_like) {
+            this.feels_like = feels_like;
+        }
+
+        public int getHumidity() {
+            return humidity;
+        }
+
+        public void setHumidity(int humidity) {
+            this.humidity = humidity;
+        }
+
+        public double getWind_speed() {
+            return wind_speed;
+        }
+
+        public void setWind_speed(double wind_speed) {
+            this.wind_speed = wind_speed;
+        }
+
+        public List<Weather> getWeather() {
+            return weather;
+        }
+
+        public void setWeather(List<Weather> weather) {
+            this.weather = weather;
         }
     }
 
@@ -74,22 +142,20 @@ public class ForecastResponse {
         private double day;
         private double night;
 
-        /**
-         * Returns the day temperature.
-         *
-         * @return temperature in degrees
-         */
         public double getDay() {
             return day;
         }
 
-        /**
-         * Returns the night temperature.
-         *
-         * @return temperature in degrees
-         */
+        public void setDay(double day) {
+            this.day = day;
+        }
+
         public double getNight() {
             return night;
+        }
+
+        public void setNight(double night) {
+            this.night = night;
         }
     }
 
@@ -99,13 +165,12 @@ public class ForecastResponse {
     public static class Weather {
         private String description;
 
-        /**
-         * Returns the textual description of the weather.
-         *
-         * @return weather description
-         */
         public String getDescription() {
             return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
         }
     }
 }
